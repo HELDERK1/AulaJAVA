dae55bef984342569c989664955483dd32ee9ee2
HELDERK1 <helderk2011@hotmail.com>
2023-02-14 22:08:15 -0300

    Primeira aula

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package helloworld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Helder, seja bem vindo ao curso de Java!");
+    }    
+}
